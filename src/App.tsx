e1ba541cffb6e9f031f192d2bbc1b719a00e6dc9
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-26 11:53:11 -0300

    update: add Sidenav and Header components on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Home } from './pages/Home/Home';
+import { Header } from './ui/components/Header/Header';
+import { Sidenav } from './ui/components/Sidenav/Sidenav';
 
 function App() {
    return (
       <>
+         <Sidenav />
+         <Header />
          <Home />
       </>
    );
