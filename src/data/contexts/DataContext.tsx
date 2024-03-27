0f7bb9b2417aee87841e2610d74d967eeb753ecd
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-27 11:35:17 -0300

    update: add a new function to user context, with name it is UseData

--- src/data/contexts/DataContext.tsx
+++ src/data/contexts/DataContext.tsx
@@ -4,6 +4,14 @@ import { DataContextProps, SalesProps } from '../@types/IDataContext';
 
 const DataContext = React.createContext<DataContextProps | null>(null);
 
+export const UseData = () => {
+   const context = React.useContext(DataContext);
+
+   if (!context) throw new Error('UseData need is on DataContextProvider');
+
+   return context;
+};
+
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
    const { data, load, error } = useFetch<SalesProps[]>(
       'https://data.origamid.dev/vendas/',
