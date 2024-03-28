16308542fa144a514d78af60c48687d6b0c426c7
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-28 10:57:22 -0300

    update: add some colors on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+   content: [],
+   theme: {
+      extend: {
+         colors: {
+            'color-1': '#463220',
+            'color-2': '#66593c',
+            'color-3': '#eceadd',
+            'color-4': '#f7f8f5',
+         },
+      },
+   },
+   plugins: [],
+};
