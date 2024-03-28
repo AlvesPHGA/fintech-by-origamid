053fe798d96bdaac3998fb35808e515325d74268
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-28 10:53:16 -0300

    update: add onChange on DataInput

--- src/ui/components/DataRange/DataRange.tsx
+++ src/ui/components/DataRange/DataRange.tsx
@@ -1,11 +1,27 @@
+import React from 'react';
 import { DataInput } from '../Form/DataInput';
 
 export const DataRange = () => {
+   const [begin, setBegin] = React.useState('');
+   const [end, setEnd] = React.useState('');
+
    return (
       <form>
          <div>
-            <DataInput legend="Inicio" id="begin" type="date" />
-            <DataInput legend="Final" id="end" type="date" />
+            <DataInput
+               legend="Inicio"
+               id="begin"
+               type="date"
+               value={begin}
+               onChange={({ target }) => setBegin(target.value)}
+            />
+            <DataInput
+               legend="Final"
+               id="end"
+               type="date"
+               value={end}
+               onChange={({ target }) => setEnd(target.value)}
+            />
          </div>
       </form>
    );
