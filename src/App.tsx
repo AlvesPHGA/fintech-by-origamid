007231e2109b39992b0515d822c966f738c18356
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-03-29 13:48:13 -0300

    update: add classname in section on app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { DataContextProvider } from './data/contexts/DataContext';
 import { Home } from './pages/Home/Home';
 import { Header } from './ui/components/Header/Header';
 import { Sidenav } from './ui/components/Sidenav/Sidenav';
+import { section } from './ui/styles/sectionStyles';
 
 function App() {
    return (
-      <section className="m-0 text-color-2 bg-color-4 font-sans">
+      <section className={section()}>
          <DataContextProvider>
             <section className="grid space-y-5 p-5">
                <Sidenav />
