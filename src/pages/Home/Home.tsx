eb8078e2aa831a3a5e9085741ce22958d344521a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-01 12:09:02 -0300

    update: add contents in home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,50 @@
+import { UseData } from '../../data/contexts/DataContext';
+
 export const Home = () => {
-   return <section>Home</section>;
+   const { data } = UseData();
+
+   if (data === null) return null;
+
+   return (
+      <section>
+         <div>
+            <div>
+               <h2>Vendas</h2>
+               <span>
+                  {data
+                     .filter((d) => d.status !== 'falha')
+                     .reduce((acc, item) => acc + item.preco, 0)
+                     .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                     })}
+               </span>
+            </div>
+            <div>
+               <h2>Recebido</h2>
+               <span>
+                  {data
+                     .filter((d) => d.status === 'pago')
+                     .reduce((acc, item) => acc + item.preco, 0)
+                     .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                     })}
+               </span>
+            </div>
+            <div>
+               <h2>Processando</h2>
+               <span>
+                  {data
+                     .filter((d) => d.status === 'processando')
+                     .reduce((acc, item) => acc + item.preco, 0)
+                     .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                     })}
+               </span>
+            </div>
+         </div>
+      </section>
+   );
 };
