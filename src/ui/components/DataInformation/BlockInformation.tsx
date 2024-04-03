ad2b5f7b666f52f3ef0cafad5ac2ae7d77a16298
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-03 17:25:24 -0300

    update: BlockInformation component

--- src/ui/components/DataInformation/BlockInformation.tsx
+++ src/ui/components/DataInformation/BlockInformation.tsx
@@ -5,7 +5,6 @@ export const BlockInformation = ({
    title: string;
    children: string;
 }) => {
-   // console.log(props.data.filter((i) => i.status !== 'falha'));
    return (
       <div className="bg-white py-2.5 px-5 rounded-[1.25rem]">
          <h2 className="text-2xl mb-2.5 text-color-1">{title}</h2>
