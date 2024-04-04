5ae6c6e1feaa8081829b040bd68c6cbdf46cb48f
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-04 11:37:32 -0300

    feat: create TableSales component to Sales

---
+++ src/ui/partials/TableSales/TableSales.tsx
@@ -0,0 +1,12 @@
+export const TableSales = () => {
+   return (
+      <table>
+         <thead>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Valor</th>
+            <th>Status</th>
+         </thead>
+      </table>
+   );
+};
