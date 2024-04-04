87816f1d7ecda2f6adfef85bc01b33a571bcaee2
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-04 11:36:53 -0300

    feat: create TableSales partial

--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,3 +1,19 @@
+import { UseData } from '../../data/contexts/DataContext';
+
 export const Sales = () => {
-   return <h1>vendas</h1>;
+   const { data } = UseData();
+
+   if (data === null) return null;
+   return (
+      <ul>
+         {data.map((d) => (
+            <li key={d.id}>
+               <span>ID: {d.id}</span>
+               <span>Nome: {d.nome}</span>
+               <span>Valor: {d.preco}</span>
+               <span>Status: {d.status}</span>
+            </li>
+         ))}
+      </ul>
+   );
 };
