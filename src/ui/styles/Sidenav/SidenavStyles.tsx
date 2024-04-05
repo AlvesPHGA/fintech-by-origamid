92e15241ac78d1bb7c68d3582a658da3fe8189c8
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-05 12:12:18 -0300

    update: add two styles to SidenavStyles

---
+++ src/ui/styles/Sidenav/SidenavStyles.tsx
@@ -0,0 +1,9 @@
+import { tv } from 'tailwind-variants';
+
+export const SidenavStyle = tv({
+   base: 'bg-color-3 rounded-[1.25rem] text-center',
+});
+
+export const logoStyle = tv({
+   base: 'block mt-5 text-6xl',
+});
