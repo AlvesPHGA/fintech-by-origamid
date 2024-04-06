c72203a4f928ffa29d88ac5fd9277f2cbd8f1657
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-06 20:40:18 -0300

    update: Months

--- src/ui/partials/Months/Months.tsx
+++ src/ui/partials/Months/Months.tsx
@@ -3,6 +3,7 @@ import { Button } from '../../components/Button/Button';
 export const Months = () => {
    return (
       <div className="flex space-x-5 mt-5">
+         <Button n={-3} />
          <Button n={-2} />
          <Button n={-1} />
          <Button n={0} />
