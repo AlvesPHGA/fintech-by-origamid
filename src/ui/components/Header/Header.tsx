eb769ee4e5a99846838ae85deee4d7607b36bec6
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-05 22:01:54 -0300

    update: add useLocation on Header to location page

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -1,10 +1,31 @@
+import React from 'react';
 import { Months } from '../../partials/Months/Months';
 import { DataRange } from '../DataRange/DataRange';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
+   const [page, setPage] = React.useState('');
+
+   const local = useLocation();
+
+   React.useEffect(() => {
+      if (local.pathname === '/') {
+         setPage('Resumo');
+         document.title = `Fintech | ${page}`;
+      } else if (local.pathname === '/sales') {
+         setPage('Vendas');
+         document.title = `Fintech | ${page}`;
+      }
+   }, [local, page]);
+
    return (
       <header>
-         <DataRange />
+         <div className="grid grid-cols-[1fr_300px] space-x-5">
+            <DataRange />
+            <div className="bg-white rounded-[1.25rem] flex items-center justify-center">
+               <h1 className="text-6xl">{page}</h1>
+            </div>
+         </div>
          <Months />
       </header>
    );
