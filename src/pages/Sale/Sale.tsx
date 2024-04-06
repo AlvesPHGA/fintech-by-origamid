e0d3b6589ea28934de0867dda08e7283081b752b
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-05 22:43:10 -0300

    update: add content in sale page

--- src/pages/Sale/Sale.tsx
+++ src/pages/Sale/Sale.tsx
@@ -1,7 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../data/hooks/useFetch';
+import { SalesProps } from '../../data/@types/IDataContext';
+
 export const Sale = () => {
+   const { id } = useParams();
+   const { data, load } = useFetch<SalesProps>(
+      `https://data.origamid.dev/vendas/${id}`,
+   );
+
+   if (data === null) return null;
    return (
-      <section>
-         <h1>Sale</h1>
+      <section className="border mt-5 space-y-5">
+         <div className="bg-white flex-1 py-2 px-5 rounded-[1.25rem] text-xl">
+            <span>ID: </span>
+            <span>{data.id}</span>
+         </div>
+         <div>
+            <span>Nome: </span>
+            <span>{data.nome}</span>
+         </div>
+         <div>
+            <span>Preço: </span>
+            <span>{data.preco}</span>
+         </div>
+         <div>
+            <span>Status: </span>
+            <span>{data.status}</span>
+         </div>
+         <div>
+            <span>Pagamento: </span>
+            <span>{data.pagamento}</span>
+         </div>
       </section>
    );
 };
