9d106ac01adc30489e91e9b622539af9cac14504
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-05 21:16:01 -0300

    update: add react-router-dom and add routes to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { DataContextProvider } from './data/contexts/DataContext';
 import { Home } from './pages/Home/Home';
 import { Sales } from './pages/Sales/Sales';
@@ -7,16 +8,20 @@ import { Sidenav } from './ui/components/Sidenav/Sidenav';
 function App() {
    return (
       <section className="bg-color-4 m-0 text-color-2 font-sans">
-         <DataContextProvider>
-            <section className="grid space-y-5 p-5 grid-cols-[300px_1fr] space-x-8 max-w-[1200px] mx-auto">
-               <Sidenav />
-               <main>
-                  <Header />
-                  <Home />
-                  <Sales />
-               </main>
-            </section>
-         </DataContextProvider>
+         <BrowserRouter>
+            <DataContextProvider>
+               <div className="grid space-y-5 p-5 grid-cols-[300px_1fr] space-x-8 max-w-[1200px] mx-auto">
+                  <Sidenav />
+                  <main>
+                     <Header />
+                     <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/sales" element={<Sales />} />
+                     </Routes>
+                  </main>
+               </div>
+            </DataContextProvider>
+         </BrowserRouter>
       </section>
    );
 }
