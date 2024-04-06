1f3cccfdd1b2caac6a5ca7aa7d67cf977d33ae34
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-06 17:54:19 -0300

    feat: create a style doc to Load component

--- src/ui/components/Load/Load.tsx
+++ src/ui/components/Load/Load.tsx
@@ -1,5 +1,5 @@
+import { loadStyle } from '../../styles/Load/loadStyles';
+
 export const Load = () => {
-   return (
-      <div className="w-40 h-40 border-8 border-l-transparent border-color-2 rounded-[50%] animate-rotateLoad mx-auto mt-10"></div>
-   );
+   return <div className={loadStyle()}></div>;
 };
---
+++ src/ui/styles/Load/loadStyles.tsx
@@ -0,0 +1,5 @@
+import { tv } from 'tailwind-variants';
+
+export const loadStyle = tv({
+   base: 'w-40 h-40 border-8 border-l-transparent border-color-2 rounded-[50%] animate-rotateLoad mx-auto mt-10',
+});
