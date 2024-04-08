2f1317627694d52d745a024683e3b3cdfb0ef795
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-08 12:03:20 -0300

    feat: create Footer to project

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import { Sales } from './pages/Sales/Sales';
 import { Header } from './ui/components/Header/Header';
 import { Sidenav } from './ui/components/Sidenav/Sidenav';
 import { Sale } from './pages/Sale/Sale';
+import { Footer } from './ui/components/Footer/Footer';
 
 function App() {
    return (
-      <section className="bg-color-4 m-0 text-color-2 font-sans">
+      <section className="bg-color-4 m-0 text-color-2 font-sans h-[100%]">
          <BrowserRouter>
             <DataContextProvider>
                <div className="grid space-y-5 p-5 grid-cols-[300px_1fr] space-x-8 max-w-[1200px] mx-auto">
@@ -24,6 +25,7 @@ function App() {
                </div>
             </DataContextProvider>
          </BrowserRouter>
+         <Footer />
       </section>
    );
 }
---
+++ src/ui/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+   return (
+      <footer className="bg-color-4 text-color-3 text-center py-4 w-[100wh] h-[4.2rem]">
+         <h3>Todos os direitos reservados</h3>
+      </footer>
+   );
+};
