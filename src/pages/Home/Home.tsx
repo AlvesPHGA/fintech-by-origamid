21a90b251a8a534dfd527df8204812a150dbe437
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-09 10:39:44 -0300

    update: add Load component on some part of project

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { UseData } from '../../data/contexts/DataContext';
+import { Load } from '../../ui/components/Load/Load';
 import { GraphicSales } from '../../ui/partials/GraphicSales/GraphicSales';
 import { Resume } from '../../ui/partials/Resume/Resume';
 
 export const Home = () => {
-   const { data } = UseData();
+   const { data, load } = UseData();
 
+   if (load) return <Load />;
    if (data === null) return null;
 
    return (
--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,9 +1,11 @@
 import { UseData } from '../../data/contexts/DataContext';
+import { Load } from '../../ui/components/Load/Load';
 import { TableSales } from '../../ui/partials/TableSales/TableSales';
 
 export const Sales = () => {
-   const { data } = UseData();
+   const { data, load } = UseData();
 
+   if (load) return <Load />;
    if (data === null) return null;
    return <TableSales />;
 };
